e8046c1b5b843e11c4de1d340d120904fd77c4e4
gupta2932 <gupta2932@gmail.com>
2018-07-17 21:54:48 +0530

    Install Live Server

---
+++ js/app.js
@@ -0,0 +1,12 @@
+(function(){
+ 
+  /*
+     * Declaration of main angular module for this application.
+     *
+     * It is named FactsandQuiz and has no dependencies (hence the 
+     * empty array as the second argument)
+     */
+    angular
+        .module("FactsandQuiz", [ ]);
+ 
+})();
